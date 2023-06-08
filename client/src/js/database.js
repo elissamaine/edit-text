ffc8bfdb6b3fc2873b0347f3241459b5797726f3
elissamaine <emaine97@yahoo.com>
2023-06-08 13:16:12 -0700

    fixed bug with batabase

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,7 +1,7 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('jate', 2, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
@@ -14,17 +14,17 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  const db = await openDB('jate');
+  const db = await openDB('jate', 2);
   const tx = db.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({jate: content});
+  const request = store.put({id: 1, jate: content});
   const result = await request;
   console.log('putDb result', result);
 }; 
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  const db = await openDB('jate', 1);
+  const db = await openDB('jate', 2);
   const tx = db.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
   const request = store.getAll();
